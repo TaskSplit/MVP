098e86a1ce5f7bcb2641e38ade29b24dbdea0991
TaskSplit <tasksplit.info@gmail.com>
2026-02-28 20:16:09 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   basePath: '/mvp',
-  output: 'export', // Add this line
   trailingSlash: true, 
 };
 
