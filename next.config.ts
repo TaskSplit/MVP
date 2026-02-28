013a253645393319a66fc7ee238d5ef9303e27da
TaskSplit <tasksplit.info@gmail.com>
2026-02-28 20:14:35 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  
-  // 1. Tells Next.js that the app is hosted under a subpath
   basePath: '/mvp',
-
-  // 2. Ensures that trailing slashes are handled consistently 
-  // (Optional, but recommended for Cloudflare Pages compatibility)
+  output: 'export', // Add this line
   trailingSlash: true, 
 };
 
