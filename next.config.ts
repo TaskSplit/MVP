ac06d26bdc98b63f9776dd7e8f083a90680fb6e6
Vivien Márton <vivien.marton@spsehalova.sk>
2026-03-02 19:17:26 +0100

    minor updates

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   basePath: "/mvp",
   trailingSlash: true,
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
